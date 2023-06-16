2df961d043d27481710cbbdc68830143209c1eb8
Lukas Dargel <lukece@mailbox.org>
2023-06-16 13:59:19 +0000

    add package and data documentation

---
+++ R/CoDaImpact-package.R
@@ -0,0 +1,39 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+
+#' Data on the rice yields in the Vietnamese provinces
+#'
+#' @description
+#' The data is used in the article by (Huong et al. 2023), who study the impact
+#' of climate change on the rice production in Vietnam. \cr
+#' It contains the following information:
+#'   +  `PROVINCE`: a factor for the 63 provinces of Vietnam
+#'   +  `REGION`: a factor with the 6 main regions
+#'   +  `YEAR`: a numeric corresponding to the year
+#'   +  `YIELD`: a numeric for the rice production in tonnes per hectare
+#'   +  `PRECIPITATION`: a numeric for the annual precipitation in liters
+#'   +  `TEMPERATURES`: a compositional variable represented as a matrix \cr
+#'       It corresponding to the fraction of days in a year where the maximal
+#'       temperature falls into one of the three categories,
+#'       "LOW", "MIDDLE" and "HIGH".
+#'
+#' @name rice_yields
+#' @docType data
+#' @author Lukas Dargel, Christine Thomas-Agnan
+#' @references
+#'   Thi-Huong Trinh, Michel Simioni, and Christine Thomas-Agnan,
+#'   “Discrete and Smooth Scalar-on-Density Compositional Regression
+#'   for Assessing the Impact of Climate Change on Rice Yield in Vietnam”,
+#'   TSE Working Paper, n. 23-1410, February 2023.
+#' @keywords data
+NULL
+
+
+
+
+
