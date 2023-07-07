94b08f09260e22c51afa371d5e9deec48d88f088
Luke <lukece@mailbox.org>
2023-07-07 12:17:17 +0000

    update VariationTable() and associated tests

--- R/VariationTable.R
+++ R/VariationTable.R
@@ -4,17 +4,11 @@
 #' Compute variations
 #'
 #' @details
-#' Developped in Dargel and Thomas-Agnan (2023)
+#' Developed in Dargel and Thomas-Agnan (2023)
 #'
-#' @param object
-#' @param Xvar
-#' @param Xdir
-#' @param inc_size
-#' @param inc_rate
-#' @param obs
-#' @param Xtotal
-#'
-#' @return
+#' @inheritParams VariationScenario
+#' @param Ytotal a numeric indicating the total of Y
+#' @return data.frame
 #' @export
 #'
 #' @examples
@@ -22,154 +16,81 @@ VariationTable <- function(
     object,
     Xvar,
     Xdir,
-    Ytotal = 1,
     obs = 1,
     inc_size = .1,
-    inc_rate = NULL) {
+    inc_rate = NULL,
+    Ytotal = 1) {
 
   stopifnot(is(object, "lmCoDa"),
             is.character(Xvar) && length(Xvar) == 1,
-            is.numeric(Ytotal) && length(Ytotal) == 1,
+            missing(Xdir) || is.character(Xdir) || is.numeric(Xdir),
             length(obs) == 1,
             is.numeric(inc_size) && length(inc_size) == 1,
-            is.null(inc_rate) || (0 < inc_rate && inc_rate < 1))
+            is.null(inc_rate) || (0 < inc_rate && inc_rate < 1),
+            is.numeric(Ytotal) && length(Ytotal) == 1)
 
+  clo2 <- function(x) x/sum(x)
   trSry <- object$trSry
-  XY_is_compo <- unlist("" != trSry$LR_TRAN[c(1, Xvar)], use.names = FALSE)
-  if (all(!YX_is_compo)) stop("Variations are not meaningful when Y and X are both not compositional!")
+  if (all(trSry$LR_TRAN[c(1, Xvar)] == "")) stop("Variations are only meaningful if X or Y are compositional!")
 
   # get X0
-  Anames <- unlist(traSry$NAME_SIMPLEX)
+  Anames <- unlist(trSry$NAME_SIMPLEX)
   Xnames <- setdiff(Anames[-1], "(Intercept)")
   if (!Xvar %in% Xnames) stop("Xvar musst be one of ", list(Xnames), "!")
-  Xvar <- unlist(traSry$NAME_COORD[Xvar == Anames])
-  Dx <- traSry$D[[Xvar]]
+  Xvar <- unlist(trSry$NAME_COORD[Xvar == Anames])
+  Dx <- trSry$D[[Xvar]]
   scalar_x <- Dx == 0
-  Kx <- traSry$LR_BASE_K[[Xvar]]
   X0 <- object$model[[Xvar]]
-  X0 <- if (scalar_x) X0[obs] else clo2(exp(t(X0[obs,]) %*% t(Kx)))
+  X0 <- if (scalar_x) X0[obs] else attr(X0, "orig")[obs,]
 
   # get Y0
-  scalar_y <- traSry$D[[1]] == 0
+  scalar_y <- trSry$D[[1]] == 0
   Y0 <- if (scalar_y) fitted(object)[obs] else fitted(object, space = "simplex")[obs,]
   Y0 <- as(Y0, "vector")
 
-  if (is.character(Xdir)) {
-    # convert preferential to general direction
-    if (!isTRUE(Xdir %in%  colnames(X0))) stop("Xdir must be one of ", list(colnames(X0)), "!")
-    Xdir <- clo2(exp(colnames(X0) == Xdir)^sqrt(Dx/(Dx-1)))
-    if (!is.null(inc_rate)) inc_size <- inc_rate * Xdir && stop("To be corrected")
+
+  # define elasticities
+  elasti <- Impacts(object, trSry$NAME_SIMPLEX[[Xvar]], obs)
+  if (!scalar_x) {
+    # for compositional X we need to account for the in which X changes
+    if (is.character(Xdir)) {
+      Xvertex <- Xdir == names(X0)
+      if (sum(Xvertex) != 1) stop("When charater; Xdir must be one of ", list(names(X0)), "!")
+
+      Xdir <- clo2(exp(Xvertex)^sqrt(Dx/(Dx-1)))
+      if (!is.null(inc_rate)) inc_size <- inc_rate * log(Xdir)[Xvertex]
+    } else {
+      valid_dir <- length(Xdir) == length(X0) && all(Xdir > 0)
+      if (!valid_dir) stop("When numeric; Xdir must be a positive vector of length ", length(X0), "!")
+
+      Xdir <- ilr(Xdir)
+      Xdir <- as(ilrInv(Xdir/sqrt(sum(Xdir^2))),"vector")
+    }
+    inc_rate <- as(log(Xdir)*inc_size, "vector")
+    elasti   <- log(Xdir) %*% elasti
   }
 
+  Ytotal <- if (scalar_y) 1 else Ytotal
+  Y1  <- Y0 * (1 + elasti * inc_size)
+  YD  <- Y1 - Y0
+  explanation <- c(
+    "Y0"        = if (scalar_y) "Initial value" else "Initial parts",
+    "Y1"        = if (scalar_y) "New value" else "New parts",
+    "elasti"    = if (scalar_y || scalar_x) "Semi elasticity" else "Elasticity",
+    "YD/Y0*100" = "Variation in %",
+    "YD*100"    = if (scalar_y) NULL else "Variation in % points",
+    "YD*Ytotal" = if (scalar_y || Ytotal != 1) "Variation in units" else NULL)
 
-  elasti <- Impacts(object, Xvar, obs)
-  elasti <- elasti %*% log(Xdir)
-  Y1     <- Y0 * (1 + elasti * inc_size)
-  diff_Y <- Y1 - Y0
-  result <- data.frame(
-    "Elasticity"            = elasti,
-    "Initial parts"         = Y0,
-    "New parts"             = Y1,
-    "Variation in %"        = diff_Y/Y0 * 100,
-    "Variation in % points" = diff_Y    * 100,
-    "Variation in units"    = diff_Y*Ytotal)
+  evalString <- function(s) eval(str2lang(s), environment())
+  explanation <- explanation[!is.null(explanation)]
+  result <- lapply(names(explanation), evalString)
+  result <- data.frame(Reduce("rbind", result), row.names = explanation)
 
-
-
-  attr(result,'X(0)')        <- X0
-  attr(result,'Xdirection')  <- Xdir
-  attr(result,'inc_size')    <- inc_size
-  attr(result,'inc_rate')    <- inc_rate
-
-
+  if (scalar_x) Xdir <- inc_rate <- NULL
+  attr(result,'X(0)')     <- X0
+  attr(result,'Xdir')     <- Xdir
+  attr(result,'inc_size') <- inc_size
+  attr(result,'inc_rate') <- inc_rate
+  return(result)
 }
 
-Variation_Table <- function(object, Var_Name, Xdirection, Inc_rate, Inc_size, Obs=1 , Total_obs=1){
-  #Remark: add alpha's computation for both cases
-  Var_Names_COORD <- names(which(object$ToSimplex$TSR$NAME_SIMPLEX==Var_Name))
-  Dx <- object$ToSimplex$TSR$D[[Var_Names_COORD]]
-  MS<- model.simplex(object)
-
-  }else{
-    if (is.character(Xdir)){
-      n=which(rownames(elasti)==Xdir)
-      Inc_size          <- Inc_rate/(1-model.simplex(object)[Obs,Xvar][n])
-    }else{if (is.numeric(Xdir)){
-      Inc_size          <- Inc_rate/(sqrt(Dx/(Dx-1))*(1-mean(model.simplex(object)[Obs,Xvar])))}
-    }
-  }
-  }
-  if ((Dx > 0) & is.character(Xdir)){
-    if (Xdir %in% colnames(MS[[Xvar]])){
-      k=which(colnames(MS[[Xvar]])==Xdir)
-    }else{
-      stop("As a character, Xdirection must be one of the following", list(colnames(MS[[Xvar]])))
-    }
-    if (k==1){Xdir <- c(exp(1),rep(1,Dx-1))
-    }else{if (k==Dx){Xdir <- c(rep(1,Dx-1),exp(1))
-    }else{Xdir <- c(rep(1,k-1),exp(1),rep(1,Dx-k))}
-    }
-  }
-  if(is.numeric(Xdir)){
-    Xdir <- ilrInv((ilr(Xdir))/(norm(ilr(Xdir))))
-  }
-  if (is.null(ncol(elasti)) | ncol(elasti)==1){
-    Table <- c()
-    Sum=0
-    for (m in 1:length(Xdir)){
-      Sum = Sum + (elasti[m])*log(Xdir[m])
-    }
-    Sum1 = Inc_size * Sum
-    Table <- (fity[Obs])*(1 + Sum1)
-    Table <- as.matrix(Table)
-    Table <- rbind(Sum,
-                   fity[Obs],
-                   Table,
-                   (Table-as.numeric(fity[Obs])))
-    rownames(Table)           <- c('Elasticity','Initial value','New value','Variation in units')
-    colnames(Table)           <- colnames(coefficients.lmSimplex(object)$Coefficients_SIMPLEX)
-  }
-  else{
-    if (nrow(elasti)==1){
-      Table <- c()
-      for (j in colnames(coefficients.lmSimplex(object)$Coefficients_SIMPLEX)){
-        Prod<-Inc_size*(elasti[,j])
-        Table[j] <- (fity[Obs][j])*(1 + Prod)
-      }
-      Table <- t(as.matrix(Table))
-      Table <- rbind(elasti,
-                     fity[Obs],
-                     Table,
-                     (Table-as.numeric(fity[Obs]))/as.numeric(fity[Obs])*100,
-                     (Table-as.numeric(fity[Obs]))*100,
-                     (Table-as.numeric(fity[Obs]))*Total_obs)
-      rownames(Table)           <- c('Elasticity','Initial parts','New parts','Variation in %','Variation in % points','Variation in units')
-    }
-    else{
-      Table <- c()
-      ELASTICITY  <- list()
-      for (j in colnames(coefficients.lmSimplex(object)$Coefficients_SIMPLEX)){
-        Sum=0
-        for (m in 1:length(Xdir)){
-          Sum = Sum + (elasti[m,j])*log(Xdir[m])
-        }
-        Sum1 = Inc_size * Sum
-        Table[j] <- (fity[Obs,j])*(1 + Sum1)
-        ELASTICITY[j]<-Sum
-      }
-      Table <- t(as.matrix(Table))
-      Table <- rbind(ELASTICITY,
-                     fity[Obs],
-                     Table,
-                     (Table-as.numeric(fity[Obs]))/as.numeric(fity[Obs])*100,
-                     (Table-as.numeric(fity[Obs]))*100,
-                     (Table-as.numeric(fity[Obs]))*Total_obs)
-      rownames(Table)           <- c('Elasticity','Initial parts','New parts','Variation in %','Variation in % points','Variation in units')
-    }
-  }
-  attr(Table,'X(0)')        <- MS[Obs,Xvar]
-  attr(Table,'Xdirection')  <- ifelse(nrow(elasti)==1,rownames(elasti),Xdir)
-  attr(Table,'Inc_size')    <- Inc_size
-  attr(Table,'Inc_rate')    <- ifelse (missing(Inc_rate),NA,Inc_rate)
-  return(Table)
-}
